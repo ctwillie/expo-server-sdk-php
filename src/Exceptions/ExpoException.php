43539e62f779a663538e953b0d9a03b1ba1062b1
Marcin Sadowski <marcin.sadowski@billennium.pl>
2023-06-21 12:06:44 +0200

    return type cannot be declared

--- src/Exceptions/ExpoException.php
+++ src/Exceptions/ExpoException.php
@@ -6,9 +6,8 @@ class ExpoException extends \Exception {
 
     public $details = null;
 
-    public function __construct(string $message = "", int $code = 0, ?\Throwable $previous = null, $details = null): \Exception {
+    public function __construct(string $message = "", int $code = 0, ?\Throwable $previous = null, $details = null) {
         $this->details = $details;
         parent::__construct($message, $code, $previous);
     }
-
 }
